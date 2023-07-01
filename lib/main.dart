e139b46611519023d9b30587135b1959be0b93f7
kisyam <how2logout@gmail.com>
2023-07-02 00:02:05 +0900

    darkMode changed

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:expense_tracker/widgets/expenses.dart';
 final kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
-final kDarkColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 99, 125));
+final kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
@@ -15,10 +17,16 @@ void main() {
         useMaterial3: true,
         colorScheme: kDarkColorScheme,
         cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
+          color: kDarkColorScheme.secondaryContainer,
           margin: const EdgeInsets.symmetric(
             horizontal: 16,
             vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
           ),
         ),
       ),
